5cdb9cb4d2955373cf57cac2a4f78a509d39b693
Christian Oerschkes <christian.oerschkes@hotmail.de>
2024-03-29 13:34:48 +0100

    feat: Add a dashboard-component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import {Routes} from '@angular/router';
 import {AuthComponent} from "./core/auth/auth.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
 
-export const routes: Routes = [{path: 'auth', component: AuthComponent}];
+export const routes: Routes = [{path: 'auth', component: AuthComponent}, {path: 'dashboard', component: DashboardComponent}];
