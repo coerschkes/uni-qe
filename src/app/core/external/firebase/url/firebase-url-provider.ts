4aecbf0078cedcde425774012b6caa2027d18c90
Christian Oerschkes <christian.oerschkes@hotmail.de>
2024-04-02 00:30:02 +0200

    feat: Change login method to log in with password for now.

--- src/app/core/external/firebase/url/firebase-url-provider.ts
+++ src/app/core/external/firebase/url/firebase-url-provider.ts
@@ -4,7 +4,7 @@ import {SecureTokenDomain, SecureTokenUrl} from "./secure-token-url";
 
 export class FirebaseUrlProvider {
   public static login(): string {
-    return new IdentityToolkitUrl(IdentityToolkitDomain.ACCOUNTS, IdentityToolkitActions.LOGIN_WITH_CUSTOM_TOKEN, environment.FIREBASE_API_KEY).toString()
+    return new IdentityToolkitUrl(IdentityToolkitDomain.ACCOUNTS, IdentityToolkitActions.LOGIN_WITH_PASSWORD, environment.FIREBASE_API_KEY).toString()
   }
 
   public static signUp(): string {
