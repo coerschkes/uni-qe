c39ede145aec62beb0e5f6c175cf1c9d68bc38f6
Christian Oerschkes <christian.oerschkes@hotmail.de>
2024-04-02 14:35:31 +0200

    feat: Add Angular material, enable material 3 and start restyling teh auth component.

--- src/app/core/auth/component/auth.component.ts
+++ src/app/core/auth/component/auth.component.ts
@@ -1,26 +1,50 @@
 // noinspection JSIgnoredPromiseFromCall
 
 import {Component, Signal} from '@angular/core';
-import {FormsModule, NgForm} from "@angular/forms";
+import {FormControl, FormGroupDirective, FormsModule, NgForm} from "@angular/forms";
 import {AuthService} from "../auth.service";
 import {AuthComponentStateService} from "./auth.component.state.service";
 import {Router} from "@angular/router";
 import {catchError, finalize, first, Observable, of, tap} from "rxjs";
+import {MatButton} from "@angular/material/button";
+import {MatSidenav, MatSidenavContainer, MatSidenavContent} from "@angular/material/sidenav";
+import {MatError, MatFormField, MatHint, MatLabel} from "@angular/material/form-field";
+import {ErrorStateMatcher} from "@angular/material/core";
+import {MatInput} from "@angular/material/input";
+import {NgIf} from "@angular/common";
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-auth',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    MatButton,
+    MatSidenavContent,
+    MatSidenav,
+    MatSidenavContainer,
+    MatFormField,
+    MatError,
+    MatHint,
+    MatLabel,
+    MatInput,
+    NgIf
   ],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.css'
+  styleUrl: './auth.component.scss'
 })
 //todo: style
 export class AuthComponent {
   private readonly authService: AuthService
   private readonly authComponentStateService: AuthComponentStateService
   private readonly router: Router
+  matcher = new MyErrorStateMatcher();
 
   constructor(authService: AuthService, authComponentStateService: AuthComponentStateService, router: Router) {
     this.authService = authService
@@ -83,5 +107,9 @@ export class AuthComponent {
           this.authComponentStateService.switchLoading()
         }),
       ).subscribe()
+  }
+
+  public log(form: NgForm):void{
+    console.log(form)
   }
 }
